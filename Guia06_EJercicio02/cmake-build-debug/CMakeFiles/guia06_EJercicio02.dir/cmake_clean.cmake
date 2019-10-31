a9954b134bd7b2742375ada1423da1d01caf4538
Natalia Sofia Martinez Lima <natasha.fifi98@gmail.com>
2019-10-31 17:34:00 -0600

    la guia 6

---
+++ Guia06_EJercicio02/cmake-build-debug/CMakeFiles/guia06_EJercicio02.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/guia06_EJercicio02.dir/main.c.o"
+  "guia06_EJercicio02"
+  "guia06_EJercicio02.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/guia06_EJercicio02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ejemplo2pilas/cmake-build-debug/CMakeFiles/ejemplo2pilas.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ejemplo2pilas.dir/main.c.o"
+  "ejemplo2pilas.pdb"
+  "ejemplo2pilas"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ejemplo2pilas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
